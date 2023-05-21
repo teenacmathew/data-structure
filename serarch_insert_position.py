e9611c9ec44ee9c34627fb8b3fcf44072ffbb662
Teena Mathew <teencmathew93@gmail.com>
2023-05-21 14:09:00 -0600

    search insert position

---
+++ serarch_insert_position.py
@@ -0,0 +1,24 @@
+"""
+Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if target <= nums[mid]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
