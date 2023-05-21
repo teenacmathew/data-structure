6c1a48efd0b24c29a9ff248209809adc78c108cf
Teena Mathew <teencmathew93@gmail.com>
2023-05-21 10:51:26 -0600

    remove linked list element

---
+++ remove-linked-list-element.py
@@ -0,0 +1,23 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+
+        while head and head.val == val:
+            head = head.next
+
+        current = head
+
+        # Second loop
+        while current:
+            while current and current.next and current.next.val == val:
+                current.next = current.next.next
+            current = current.next
+        return head
