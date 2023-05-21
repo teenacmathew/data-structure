7f67d8509617038f51f2c5947a667519b7267519
Teena Mathew <teencmathew93@gmail.com>
2023-05-21 14:20:22 -0600

    delete node in linked list

---
+++ delete-node-in-linked-list.py
@@ -0,0 +1,43 @@
+"""
+Delete Node in a Linked List
+
+There is a singly-linked list head and we want to delete a node node in it.
+
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before node should be in the same order.
+All the values after node should be in the same order.
+
+"""
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val=node.next.val
+        if node and node.next:
+            node.next = node.next.next
+            return
+        node.next = null
+
+
+
+
+
+
+
+
